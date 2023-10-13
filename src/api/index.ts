4cd43dc7c413d9f600de59db522a860205ad08e3
FajarKim <PenghapusJawaban@gmail.com>
2023-10-14 05:40:03 +0700

    feat: Added error message card

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import getData from "../src/getData";
 import cardStyle from "../src/card";
 import { themes, Themes } from "../../themes/index";
+import { renderError, SECONDARY_ERROR_MESSAGES } from "../src/utils";
 
 export type UiConfig = {
   titleColor: string;
@@ -30,7 +31,9 @@ export default async function readmeStats(req: any, res: any): Promise<any> {
       bgColor: req.query.bg_color || selectTheme.bg_color || defaultTheme.bg_color,
     };
 
-    if (!username) throw new Error("Username is required", "");
+    if (!username) { 
+      return res.send(renderError("Something went wrong", SECONDARY_ERROR_MESSAGES.USER_NOT_FOUND));
+    }
 
     var fetchStats = await getData(username);
     res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
