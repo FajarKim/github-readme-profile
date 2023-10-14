40fd19949dd931a237eb76e6d45d36316d8c720e
Rangga Fajar Oktariansyah <86386385+FajarKim@users.noreply.github.com>
2023-10-14 08:51:13 +0700

    feat: Added locale option (#21)

---
+++ src/src/translations.ts
@@ -0,0 +1,31 @@
+export type Locales = {
+  [key: string]: {
+    titleCard: string;
+    followersText: string;
+    followingText: string;
+    totalReposText: string;
+    starsCountText: string;
+    forksCountText: string;
+    commitsCountText: string;
+    totalPRText: string;
+    totalPRMergedText: string;
+    totalIssuesText: string;
+    contributedToText: string;
+  };
+};
+
+export const locales: Locales = {
+  en: {
+    titleCard: "{name}'s GitHub Stats",
+    followersText: "Followers",
+    followingText: "Following",
+    totalReposText: "Total Repository",
+    starsCountText: "Star's Count"
+    forksCountText: "Fork's Count",
+    commitsCountText: "Commit's Count",
+    totalPRText: "Total PRs",
+    totalPRMergedText: "Total PRs Merged",
+    totalIssuesText: "Total Issues",
+    contributedToText: "Contributed to (last year)",
+  },
+}
