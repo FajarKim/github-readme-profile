509789286adb4080904ffb538a35f1bd7ef25283
FajarKim <PenghapusJawaban@gmail.com>
2023-10-14 15:58:27 +0700

    fix: animation duration for list stats

--- src/src/card.ts
+++ src/src/card.ts
@@ -188,9 +188,9 @@ export default function cardStyle(data: GetData, uiConfig: UiConfig): string {
             </g>
 
             <g transform="translate(230, 150)">
-                <g class="single-item-animation" style="animation-delay: 760ms" transform="translate(25, 0)">
+                <g class="single-item-animation" style="animation-delay: 860ms" transform="translate(25, 0)">
                     <svg x="${iconXAngle}" y="0" class="icon" viewBox="0 0 16 16" version="1.1" width="16" height="16">
-                        <path fill-rule="evenodd" d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm9 3a1 1 0 11-2 0 1 1 0 012 0zm-.25-6.25a.75.75 0 00-1.5 0v3.5a.75.75 0 001.5 0v-3.5z" />
+                        <path fill-rule="evenodd" d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm9 3a1 1 0 11-2 0 1 1 0 012 0zm-.25-6.25a.75.75 0 00-1.5 0v3.5a.75.75 0 001.5 0v-3.5z"/>
                     </svg>
                     <text class="text" x="${textXAngle}" y="12.5">${selectLocale.totalIssuesText}:</text>
                     <text class="text text-bold" x="${dataXAngle}" y="12.5">${data.total_issues}</text>
@@ -198,7 +198,7 @@ export default function cardStyle(data: GetData, uiConfig: UiConfig): string {
             </g>
             
             <g transform="translate(230, 175)">
-                <g class="single-item-animation" style="animation-delay: 760ms" transform="translate(25, 0)">
+                <g class="single-item-animation" style="animation-delay: 960ms" transform="translate(25, 0)">
                     <svg x="${iconXAngle}" y="0" class="icon" viewBox="0 0 16 16" version="1.1" width="16" height="16">
                         <path fill-rule="evenodd" d="M2 5.5a3.5 3.5 0 1 1 5.898 2.549 5.508 5.508 0 0 1 3.034 4.084.75.75 0 1 1-1.482.235 4 4 0 0 0-7.9 0 .75.75 0 0 1-1.482-.236A5.507 5.507 0 0 1 3.102 8.05 3.493 3.493 0 0 1 2 5.5ZM11 4a3.001 3.001 0 0 1 2.22 5.018 5.01 5.01 0 0 1 2.56 3.012.749.749 0 0 1-.885.954.752.752 0 0 1-.549-.514 3.507 3.507 0 0 0-2.522-2.372.75.75 0 0 1-.574-.73v-.352a.75.75 0 0 1 .416-.672A1.5 1.5 0 0 0 11 5.5.75.75 0 0 1 11 4Zm-5.5-.5a2 2 0 1 0-.001 3.999A2 2 0 0 0 5.5 3.5Z"/>
                     </svg>
