6e882754aa024d70c67594af0f2c4f8067b07c57
FajarKim <penghapusjawaban@gmail.com>
2023-11-04 11:13:05 +0700

    refactor: revise code for improved readability and maintainability

--- src/getRandomToken.ts
+++ src/getRandomToken.ts
@@ -1,12 +1,12 @@
 require("dotenv").config();
 
 function getRandomToken(bearerHeader: boolean): string {
-  let getEnvs: any = process.env;
+  const getEnvs: any = process.env;
   let getGhEnv: any = Object.keys(getEnvs).filter((key) =>
     key.startsWith("GH_")
   );
   getGhEnv = getGhEnv.map((key: string) => getEnvs[key]);
-  let getRandomEnv: string =
+  const getRandomEnv: string =
     getGhEnv[Math.floor(Math.random() * getGhEnv.length)];
 
   if (bearerHeader) {
