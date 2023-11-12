aef71d8b5e973a8157eeb64435a2c8c30fa34de6
FajarKim <PenghapusJawaban@gmail.com>
2023-11-12 16:49:05 +0700

    fix: update script for push readme theme

--- scripts/push-theme-readme.sh
+++ scripts/push-theme-readme.sh
@@ -2,14 +2,11 @@
 set -x
 set -e
 
-export BRANCH_NAME=updated-theme-readme
 git --version
 git config --global user.email "86386385+FajarKim@users.noreply.github.com"
 git config --global user.name "Rangga Fajar Oktariansyah"
 git config --global --add safe.directory ${GITHUB_WORKSPACE}
-git branch -d $BRANCH_NAME || true
-git checkout -b $BRANCH_NAME
 git add --all
 git commit --message "docs(theme): auto update theme readme"
-git remote add origin-$BRANCH_NAME https://${PERSONAL_TOKEN}@github.com/${GH_REPO}.git
-git push --force --quiet --set-upstream origin-$BRANCH_NAME $BRANCH_NAME
+git remote add master https://${PERSONAL_TOKEN}@github.com/${GH_REPO}.git
+git push -f
