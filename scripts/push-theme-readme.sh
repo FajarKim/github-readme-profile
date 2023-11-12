12244c035f6e9a57f8c0109803f08fdcc66290e2
FajarKim <PenghapusJawaban@gmail.com>
2023-11-12 14:59:02 +0700

    ci(workflows): fixed generate theme readme

--- scripts/push-theme-readme.sh
+++ scripts/push-theme-readme.sh
@@ -2,10 +2,14 @@
 set -x
 set -e
 
+export BRANCH_NAME=updated-theme-readme
 git --version
 git config --global user.email "86386385+FajarKim@users.noreply.github.com"
 git config --global user.name "Rangga Fajar Oktariansyah"
 git config --global --add safe.directory ${GITHUB_WORKSPACE}
+git branch -d $BRANCH_NAME || true
+git checkout -b $BRANCH_NAME
 git add --all
 git commit --message "docs(theme): auto update theme readme"
-git push -f
+git remote add origin-$BRANCH_NAME https://${PERSONAL_TOKEN}@github.com/${GH_REPO}.git
+git push --force --quiet --set-upstream origin-$BRANCH_NAME $BRANCH_NAME
