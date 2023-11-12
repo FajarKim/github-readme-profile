1450cf8fb98ac0d31494d5f672854e3758231c7c
FajarKim <PenghapusJawaban@gmail.com>
2023-11-12 14:50:17 +0700

    ci(workflows): fixed generate theme readme

--- scripts/push-theme-readme.sh
+++ scripts/push-theme-readme.sh
@@ -7,6 +7,5 @@ git config --global user.email "86386385+FajarKim@users.noreply.github.com"
 git config --global user.name "Rangga Fajar Oktariansyah"
 git config --global --add safe.directory ${GITHUB_WORKSPACE}
 git add --all
-git commit --no-verify --message "docs(theme): auto update theme readme"
-git remote add master https://${PERSONAL_TOKEN}@github.com/${GH_REPO}.git
+git commit --message "docs(theme): auto update theme readme"
 git push -f
