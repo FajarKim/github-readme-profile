fa35af0b2f2839d511204d767a4f7c00fdcb7613
FajarKim <PenghapusJawaban@gmail.com>
2023-11-12 14:18:40 +0700

    fix: update script for push-readme-theme.sh

--- scripts/push-theme-readme.sh
+++ scripts/push-theme-readme.sh
@@ -3,8 +3,8 @@ set -x
 set -e
 
 git --version
-git config --global user.email "github-actions[bot]@users.noreply.github.com"
-git config --global user.name "GitHub Actions"
+git config --global user.email "86386385+FajarKim@users.noreply.github.com"
+git config --global user.name "Rangga Fajar Oktariansyah"
 git config --global --add safe.directory ${GITHUB_WORKSPACE}
 git add --all
 git commit --no-verify --message "docs(theme): auto update theme readme"
