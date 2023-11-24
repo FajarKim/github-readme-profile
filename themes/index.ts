a1865d6929528b8745355413ab7ce1e23596ed34
Xavier Nichole <146318113+XavierNotFound@users.noreply.github.com>
2023-11-24 17:39:40 +0700

    feat(theme): Added radical theme

--- themes/index.ts
+++ themes/index.ts
@@ -237,6 +237,12 @@ export const themes: Themes = {
     bg_color: "262626",
     username_color: "ffffaf",
   },
+  radical: {
+    title_color: "fe428e",
+    icon_color: "f8d847",
+    text_color: "a9fef7",
+    bg_color: "141321",
+  },
 
   // Gradient themes
   "sunset-gradient": {
