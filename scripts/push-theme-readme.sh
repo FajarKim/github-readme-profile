4fad28b7668100b883096cde77afe16ea84d1112
Rangga Fajar Oktariansyah <86386385+FajarKim@users.noreply.github.com>
2023-11-26 20:46:29 +0700

    ci: update script for pushes auto update docs readme themes and translations

--- scripts/push-theme-readme.sh
+++ scripts/push-theme-readme.sh
@@ -2,11 +2,13 @@
 set -x
 set -e
 
-git --version
-git config --global user.email "86386385+FajarKim@users.noreply.github.com"
-git config --global user.name "Rangga Fajar Oktariansyah"
-git config --global --add safe.directory ${GITHUB_WORKSPACE}
-git add --all
-git commit --message "docs(theme): auto update theme readme"
-git remote add master https://${PERSONAL_TOKEN}@github.com/${GH_REPO}.git
-git push -f
+if [[ "$(git status --porcelain)" != "" ]]; then
+  git --version
+  git config --global user.email "86386385+FajarKim@users.noreply.github.com"
+  git config --global user.name "Rangga Fajar Oktariansyah"
+  git config --global --add safe.directory ${GITHUB_WORKSPACE}
+  git add --all
+  git commit --message "docs(theme): auto update theme readme"
+  git remote add master https://${PERSONAL_TOKEN}@github.com/${GH_REPO}.git
+  git push -f
+fi
