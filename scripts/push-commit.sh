3ec23866a96f133faeb794ac381e5a0743f54ef9
Rangga Fajar Oktariansyah <86386385+FajarKim@users.noreply.github.com>
2023-11-29 12:33:56 +0700

    ci(push-commit): revise code for simple ability

--- scripts/push-commit.sh
+++ scripts/push-commit.sh
@@ -3,12 +3,12 @@ set -x
 set -e
 
 git --version
-git config --global user.email "86386385+FajarKim@users.noreply.github.com"
-git config --global user.name "Rangga Fajar Oktariansyah"
+git config --global user.email "${AUTHOR_EMAIL}"
+git config --global user.name "${AUTHOR_NAME}"
 git config --global --add safe.directory ${GITHUB_WORKSPACE}
 if [[ "$(git status --porcelain)" != "" ]]; then
   git add --all
-  git commit --message "docs(translation): auto update translation readme"
+  git commit --message "${MESSAGE}"
   git remote add master https://${PERSONAL_TOKEN}@github.com/${GH_REPO}.git
   git push -f
 fi
