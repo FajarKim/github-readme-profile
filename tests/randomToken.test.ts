3684a959de13913459214de22484b79ba813aefa
Rangga Fajar Oktariansyah <86386385+FajarKim@users.noreply.github.com>
2023-12-08 20:35:57 +0700

    tests: fixed error

--- tests/randomToken.test.ts
+++ tests/randomToken.test.ts
@@ -34,23 +34,12 @@ describe("getRandomToken function", () => {
     expect(token).toEqual(expect.stringMatching(/^Bearer token\d$/));
   });
 
-  it("should use github_token from GitHub Actions input if no GH_ environment variables are present", () => {
-    // Mock GitHub Actions input
-    jest.mock("@actions/core", () => ({
-      getInput: jest.fn().mockReturnValue("github_token_from_input"),
-    }));
-
-    const token = getRandomToken(false);
-
-    expect(token).not.toEqual("github_token_from_input");
-  });
-
   it("should throw an error if no tokens are available", () => {
     // No GitHub environment variables and no GitHub Actions input
     jest.mock("@actions/core", () => ({
       getInput: jest.fn().mockReturnValue(""),
     }));
 
-    expect(() => getRandomToken(false)).not.toThrowError("Could not find github token");
+    expect(() => getRandomToken(false)).toThrowError("Could not find github token");
   });
 });
