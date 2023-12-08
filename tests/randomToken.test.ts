64af03ea667bdb6fa0e107a262ed98dafc0cef7e
Rangga Fajar Oktariansyah <86386385+FajarKim@users.noreply.github.com>
2023-12-08 20:13:18 +0700

    fixed style code

--- tests/randomToken.test.ts
+++ tests/randomToken.test.ts
@@ -1,10 +1,10 @@
-import getRandomToken from '../src/getRandomToken'; // Adjust the import path based on your project structure
+import getRandomToken from "../src/getRandomToken";
 
 // Mock the dependencies
-jest.mock('dotenv');
-jest.mock('@actions/core');
+jest.mock("dotenv");
+jest.mock("@actions/core");
 
-describe('getRandomToken function', () => {
+describe("getRandomToken function", () => {
   // Mock process.env values
   const originalEnv = process.env;
   beforeEach(() => {
@@ -14,43 +14,43 @@ describe('getRandomToken function', () => {
     process.env = originalEnv;
   });
 
-  it('should return a random token without Bearer prefix', () => {
+  it("should return a random token without Bearer prefix", () => {
     // Mock GitHub environment variables
-    process.env.GH_TOKEN_1 = 'token1';
-    process.env.GH_TOKEN_2 = 'token2';
+    process.env.GH_TOKEN_1 = "token1";
+    process.env.GH_TOKEN_2 = "token2";
 
     const token = getRandomToken(false);
 
     expect(token).toEqual(expect.stringMatching(/^token\d$/));
   });
 
-  it('should return a random token with Bearer prefix', () => {
+  it("should return a random token with Bearer prefix", () => {
     // Mock GitHub environment variables
-    process.env.GH_TOKEN_1 = 'token1';
-    process.env.GH_TOKEN_2 = 'token2';
+    process.env.GH_TOKEN_1 = "token1";
+    process.env.GH_TOKEN_2 = "token2";
 
     const token = getRandomToken(true);
 
     expect(token).toEqual(expect.stringMatching(/^Bearer token\d$/));
   });
 
-  it('should use github_token from GitHub Actions input if no GH_ environment variables are present', () => {
+  it("should use github_token from GitHub Actions input if no GH_ environment variables are present", () => {
     // Mock GitHub Actions input
-    jest.mock('@actions/core', () => ({
-      getInput: jest.fn().mockReturnValue('github_token_from_input'),
+    jest.mock("@actions/core", () => ({
+      getInput: jest.fn().mockReturnValue("github_token_from_input"),
     }));
 
     const token = getRandomToken(false);
 
-    expect(token).not.toEqual('github_token_from_input');
+    expect(token).not.toEqual("github_token_from_input");
   });
 
-  it('should throw an error if no tokens are available', () => {
+  it("should throw an error if no tokens are available", () => {
     // No GitHub environment variables and no GitHub Actions input
-    jest.mock('@actions/core', () => ({
-      getInput: jest.fn().mockReturnValue(''),
+    jest.mock("@actions/core", () => ({
+      getInput: jest.fn().mockReturnValue(""),
     }));
 
-    expect(() => getRandomToken(false)).not.toThrowError('Could not find github token');
+    expect(() => getRandomToken(false)).not.toThrowError("Could not find github token");
   });
 });
