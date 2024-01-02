773b0b4412e82853c9cc40107f7cc1d984f9968c
Rangga Fajar Oktariansyah <86386385+FajarKim@users.noreply.github.com>
2024-01-02 17:43:53 +0700

    refactor: revise code for readability and maintainability

--- src/card.ts
+++ src/card.ts
@@ -24,64 +24,67 @@ export default function cardStyle(data: GetData, uiConfig: UiConfig): string {
 
   // Setting angles and positions for SVG elements
   const direction = isRtlDirection ? "rtl" : "ltr";
-  const titleXAngle = isDisabledAnimations ? (isRtlDirection ? 520 : 15) : (isRtlDirection ? 510 : 5);
-  const titleYAngle = isDisabledAnimations ? 0 : -10;
-  const textXAngle = isRtlDirection ? 215 : 25;
-  const dataXAngle = isRtlDirection ? 15 : 225;
-  const iconXAngle = isRtlDirection ? 225 : 0;
-  const imageXAngle = isDisabledAnimations ? 120 : 125;
-  const imageYAngle = isDisabledAnimations ? 70 : 65;
-  const userXAngle = isDisabledAnimations ? 119.9 : 109.9;
-  const userYAngle = isDisabledAnimations ? 140 : 130;
-  const follXAngle = isDisabledAnimations ? 120 : 110;
-  const follYAngle = isDisabledAnimations ? 161 : 151;
+  const angle = {
+    titleXAngle: isDisabledAnimations ? (isRtlDirection ? 520 : 15) : (isRtlDirection ? 510 : 5);
+    titleYAngle: isDisabledAnimations ? 0 : -10;
+    textXAngle: isRtlDirection ? 210 : 25;
+    dataXAngle: isRtlDirection ? 5 : 230;
+    iconXAngle: isRtlDirection ? 220 : 0;
+    imageXAngle: isDisabledAnimations ? 120 : 125;
+    imageYAngle: isDisabledAnimations ? 70 : 65;
+    userXAngle: isDisabledAnimations ? 119.9 : 109.9;
+    userYAngle: isDisabledAnimations ? 140 : 130;
+    follXAngle: isDisabledAnimations ? 120 : 110;
+    follYAngle: isDisabledAnimations ? 161 : 151;
+  };
 
   // Setting styles for hiding stroke and border based on UI configuration
   const hideStroke = parseBoolean(uiConfig.hideStroke) ? `` : `stroke="#${uiConfig.strokeColor}" stroke-width="5"`;
   const hideBorder = parseBoolean(uiConfig.hideBorder) ? `` : `stroke="#${uiConfig.borderColor}" stroke-opacity="1" stroke-width="${uiConfig.borderWidth}"`;
 
   // CSS animations for SVG elements
-  const animations = parseBoolean(uiConfig.disabledAnimations || uiConfig.Format === "png") ? `` : `        /* Animations */
-        @keyframes scaleInAnimation {
-            from {
-                transform: translate(-5px, 5px) scale(0);
-            }
-            to {
-                transform: translate(-5px, 5px) scale(1);
-            }
-        }
-        @keyframes fadeInAnimation {
-            from {
-                opacity: 0;
-            }
-            to {
-                opacity: 1;
-            }
-        }
-        @keyframes fadeLeftInAnimation {
-            from {
-                opacity: 0;
-                transform: translate(-90px, 10px);
-            }
-            to {
-                opacity: 1;
-                transform: translate(10px, 10px);
-            }
-        }
+  const animations = parseBoolean(uiConfig.disabledAnimations || uiConfig.Format === "png") ? `` : `
+    /* Animations */
+    @keyframes scaleInAnimation {
+      from {
+        transform: translate(-5px, 5px) scale(0);
+      }
+      to {
+        transform: translate(-5px, 5px) scale(1);
+      }
+    }
+    @keyframes fadeInAnimation {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+    @keyframes fadeLeftInAnimation {
+      from {
+        opacity: 0;
+        transform: translate(-90px, 10px);
+      }
+      to {
+        opacity: 1;
+        transform: translate(10px, 10px);
+      }
+    }
 
-        .div-animation {
-            animation: fadeLeftInAnimation 0.7s ease-in-out forwards;
-        }
+    .div-animation {
+      animation: fadeLeftInAnimation 0.7s ease-in-out forwards;
+    }
 
-        .image-profile-animation {
-            animation: scaleInAnimation 1.2s ease-in-out forwards;
-            transform-origin: ${imageXAngle}px ${imageYAngle}px;
-        }
+    .image-profile-animation {
+      animation: scaleInAnimation 1.2s ease-in-out forwards;
+      transform-origin: ${angle.imageXAngle}px ${angle.imageYAngle}px;
+    }
 
-        .single-item-animation {
-            opacity: 0;
-            animation: fadeInAnimation 0.3s ease-in-out forwards;
-        }`;
+    .single-item-animation {
+      opacity: 0;
+      animation: fadeInAnimation 0.3s ease-in-out forwards;
+    }`;
 
   // Extracting and formatting hidden and shown items based on UI configuration
   const hiddenItems = uiConfig.hiddenItems || "";
@@ -110,15 +113,15 @@ export default function cardStyle(data: GetData, uiConfig: UiConfig): string {
 
   // Generating SVG code for each visible card item
   const cardItemsSVG = cardItemsToShow.map((item, index) => `
-            <g transform="translate(230, ${index * 25})">
-                <g class="single-item-animation" style="animation-delay: ${210 + index * 100}ms" transform="translate(25, 0)">
-                    <svg x="${iconXAngle}" y="0" class="icon" viewBox="0 0 16 16" version="1.1" width="16" height="16">
-                        ${item.icon}
-                    </svg>
-                    <text class="text" x="${textXAngle}" y="12.5">${item.text}:</text>
-                    <text class="text text-bold" x="${dataXAngle}" y="12.5">${item.value}</text>
-                </g>
-            </g>`).join("\n");
+    <g transform="translate(230, ${15 + index * 25})">
+      <g class="single-item-animation" style="animation-delay: ${210 + index * 100}ms" transform="translate(25, 0)">
+        <svg x="${angle.iconXAngle}" y="0" class="icon" viewBox="0 0 16 16" version="1.1" width="16" height="16">
+          ${item.icon}
+        </svg>
+        <text class="text" x="${angle.textXAngle}" y="12.5">${item.text}:</text>
+        <text class="text text-bold" x="${angle.dataXAngle}" y="12.5">${item.value}</text>
+      </g>
+    </g>`).join("\n");
 
   /**
    * Generates a linear gradient SVG code based on an array of color stops.
@@ -135,13 +138,12 @@ export default function cardStyle(data: GetData, uiConfig: UiConfig): string {
       return `<stop offset="${offset}%" stop-color="#${color}"/>`;
     }).join("");
     return `
-    <defs>
-        <linearGradient id="${gradientId}" gradientTransform="rotate(${gradientAngle})" gradientUnits="userSpaceOnUse">
-            ${gradientStops}
-        </linearGradient>
-    </defs>
-    <rect x="0.5" y="0.5" rx="${uiConfig.borderRadius}" height="99.6%" width="99.8%" fill="url(#${gradientId})" ${hideBorder}/>
-    `;
+  <defs>
+    <linearGradient id="${gradientId}" gradientTransform="rotate(${gradientAngle})" gradientUnits="userSpaceOnUse">
+      ${gradientStops}
+    </linearGradient>
+  </defs>
+  <rect x="0.5" y="0.5" rx="${uiConfig.borderRadius}" height="99.6%" width="99.8%" fill="url(#${gradientId})" ${hideBorder}/>`;
   }
 
   // Generating SVG code for the background based on background color or gradient
@@ -155,81 +157,78 @@ export default function cardStyle(data: GetData, uiConfig: UiConfig): string {
         const gradientColors = gradientHexArray.map(color => color.trim());
         backgroundSVG = generateGradient(gradientColors);
       } else {
-        backgroundSVG = `<rect x="0.5" y="0.5" rx="${uiConfig.borderRadius}" height="99.6%" width="99.8%" fill="#${uiConfig.bgColor}" ${hideBorder}/>`;
+        backgroundSVG = `
+  <rect x="0.5" y="0.5" rx="${uiConfig.borderRadius}" height="99.6%" width="99.8%" fill="#${uiConfig.bgColor}" ${hideBorder}/>`;
       }
     }
   }
 
   // Final SVG code for the GitHub stats card
-  return `<svg width="535" height="${Math.max(220, 45 + cardItemsToShow.length * 25)}"  direction="${direction}" viewBox="0 0 535 ${Math.max(220, 45 + cardItemsToShow.length * 25)}" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <style>
-        ${animations}
+  return `
+<svg width="535" height="${Math.max(220, 45 + cardItemsToShow.length * 25)}"  direction="${direction}" viewBox="0 0 535 ${Math.max(220, 45 + cardItemsToShow.length * 25)}" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <style>
+${animations}
+    .text {
+      font-family: "Segoe UI", Ubuntu, sans-serif;
+      fill: #${uiConfig.textColor};
+      font-size: 14px;
+    }
 
-        .text {
-            font-family: "Segoe UI", Ubuntu, sans-serif;
-            fill: #${uiConfig.textColor};
-            font-size: 14px;
-        }
+    .text-bold {
+      font-weight: 700;
+    }
 
-        .text-bold {
-            font-weight: 700;
-        }
+    .text-middle {
+      alignment-baseline: middle;
+      text-anchor: middle;
+    }
 
-        .text-middle {
-            alignment-baseline: middle;
-            text-anchor: middle;
-        }
+    .text-followers {
+      font-family: "Segoe UI", Ubuntu, sans-serif;
+      fill: #${uiConfig.textColor};
+      font-size: 13px;
+    }
 
-        .text-followers {
-            font-family: "Segoe UI", Ubuntu, sans-serif;
-            fill: #${uiConfig.textColor};
-            font-size: 13px;
-        }
+    .text-username {
+      font-family: "Segoe UI", Ubuntu, sans-serif;
+      fill: #${uiConfig.usernameColor};
+      font-weight: 750;
+      font-size: 14.6px;
+      alignment-baseline: middle;
+      text-anchor: middle;
+    }
 
-        .text-username {
-            font-family: "Segoe UI", Ubuntu, sans-serif;
-            fill: #${uiConfig.usernameColor};
-            font-weight: 750;
-            font-size: 14.6px;
-            alignment-baseline: middle;
-            text-anchor: middle;
-        }
+    .text-title {
+      font-family: "Segoe UI", Ubuntu, sans-serif;
+      fill: #${uiConfig.titleColor};
+      font-size: 17px;
+      font-weight: 600;
+    }
 
-        .text-title {
-            font-family: "Segoe UI", Ubuntu, sans-serif;
-            fill: #${uiConfig.titleColor};
-            font-size: 17px;
-            font-weight: 600;
-        }
-
-        .icon {
-            fill: #${uiConfig.iconColor};
-            display: block;
-        }
-    </style>
-    <title id="titleId">${selectLocale.titleCard.split("{name}").join(data.name) || defaultLocale.titleCard.split("{name}").join(data.name)}</title>
-
-    ${backgroundSVG}
-    <g transform="translate(0, 25)">
-        <g class="div-animation">
-            <text x="${titleXAngle}" y="${titleYAngle}" class="text-title">${selectLocale.titleCard.split("{name}").join(data.name) || defaultLocale.titleCard.split("{name}").join(data.name)}</text>
-        </g>
-        <g class="image-profile-animation">
-            <defs>
-                <pattern id="image" x="0%" y="0%" height="100%" width="100%" viewBox="0 0 512 512">
-                    <image x="0%" y="0%" width="512" height="512" href="data:image/jpeg;base64,${data.pic}"></image>
-                </pattern>
-            </defs>
-            <circle cx="${imageXAngle}" cy="${imageYAngle}" r="50" fill="url(#image)" ${hideStroke}/>
-        </g>
-        <text x="${userXAngle}" y="${userYAngle}" direction="ltr" class="text-username div-animation">@${data.username}</text>
-        <g class="div-animation text-middle">
-            <text x="${follXAngle}" y="${follYAngle}" class="text-followers"><tspan class="text-bold">${data.followers}</tspan> ${selectLocale.followersText || defaultLocale.followersText} · <tspan class="text-bold">${data.following}</tspan> ${selectLocale.followingText || defaultLocale.followingText}</text>
-        </g>
-
-        <svg x="-10" y="12">
-            ${cardItemsSVG}
-        </svg>
+    .icon {
+      fill: #${uiConfig.iconColor};
+      display: block;
+    }
+  </style>
+  <title id="titleId">${selectLocale.titleCard.split("{name}").join(data.name) || defaultLocale.titleCard.split("{name}").join(data.name)}</title>
+${backgroundSVG}
+  <g transform="translate(0, 25)">
+    <g class="div-animation">
+      <text x="${angle.titleXAngle}" y="${angle.titleYAngle}" class="text-title">${selectLocale.titleCard.split("{name}").join(data.name) || defaultLocale.titleCard.split("{name}").join(data.name)}</text>
     </g>
+    <g class="image-profile-animation">
+      <defs>
+        <pattern id="image" x="0%" y="0%" height="100%" width="100%" viewBox="0 0 512 512">
+          <image x="0%" y="0%" width="512" height="512" href="data:image/jpeg;base64,${data.pic}"></image>
+        </pattern>
+      </defs>
+      <circle cx="${angle.imageXAngle}" cy="${angle.imageYAngle}" r="50" fill="url(#image)" ${hideStroke}/>
+    </g>
+    <text x="${angle.userXAngle}" y="${angle.userYAngle}" direction="ltr" class="text-username div-animation">@${data.username}</text>
+    <g class="div-animation text-middle">
+      <text x="${angle.follXAngle}" y="${angle.follYAngle}" class="text-followers"><tspan class="text-bold">${data.followers}</tspan> ${selectLocale.followersText || defaultLocale.followersText} · <tspan class="text-bold">${data.following}</tspan> ${selectLocale.followingText || defaultLocale.followingText}</text>
+    </g>
+${cardItemsSVG}
+  </g>
 </svg>`;
 }
