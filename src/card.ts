5c7ff3b87c33212d051bc636b9ec4dfd3f97a183
Rangga Fajar Oktariansyah <86386385+FajarKim@users.noreply.github.com>
2024-01-03 06:27:43 +0700

    fix: update x position for icon

--- src/card.ts
+++ src/card.ts
@@ -29,7 +29,7 @@ export default function cardStyle(data: GetData, uiConfig: UiConfig): string {
     titleYAngle: isDisabledAnimations ? 0 : -10,
     textXAngle: isRtlDirection ? 215 : 20,
     dataXAngle: isRtlDirection ? 15 : 220,
-    iconXAngle: isRtlDirection ? 220 : -5,
+    iconXAngle: isRtlDirection ? 225 : -5,
     imageXAngle: isDisabledAnimations ? 120 : 125,
     imageYAngle: isDisabledAnimations ? 70 : 65,
     userXAngle: isDisabledAnimations ? 119.9 : 109.9,
