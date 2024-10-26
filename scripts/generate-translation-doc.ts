c5802c5f3ab0762cc4a02076f8e04b944456a2e0
Rangga Fajar Oktariansyah <86386385+FajarKim@users.noreply.github.com>
2024-10-26 08:50:13 +0700

    chore: update for generate table

--- scripts/generate-translation-doc.ts
+++ scripts/generate-translation-doc.ts
@@ -4,21 +4,34 @@ import languageNames from "../i18n/languageNames";
 
 const TARGET_FILE = "./i18n/README.md";
 
+function getProgressColor(progress: number): string {
+  if (progress <= 20) return "#FF0000";
+  if (progress <= 40) return "#FF7F00";
+  if (progress <= 60) return "#FFFF00";
+  if (progress <= 80) return "#7FFF00";
+  return "#00FF00";
+}
+
 function generateTranslationsMarkdown(locale: string): string {
   return `${locale}`;
 }
 
-export function generateReadmeLocales(): string {
+export function generateReadmeLocales() {
   const availableLocales = Object.keys(locales);
 
   let localesListTable = "";
   for (let i = 0; i < availableLocales.length; i += 1) {
     const localesSlice = availableLocales.slice(i, i + 1);
     const row = localesSlice.map(locale => generateTranslationsMarkdown(locale)).join("");
+    
+    // Menghitung persentase progres dan warna yang sesuai
+    const progress = (Object.keys(locales[row]).length / 16) * 100;
+    const progressColor = getProgressColor(progress);
+
     localesListTable += `  <tr>
     <td><p align="center"><code>${row}</code></p></td>
     <td><p align="left">${languageNames[row]}</p></td>
-    <td><p align="center">${Object.keys(locales[row]).length / 16 * 100}%</p></td>
+    <td><p align="center"><img src="https://shapecolor.vercel.app/?width=14&height=14&radius=7&color=${progressColor}"/> ${progress.toFixed(2)}%</p></td>
   </tr>\n`;
   }
 
